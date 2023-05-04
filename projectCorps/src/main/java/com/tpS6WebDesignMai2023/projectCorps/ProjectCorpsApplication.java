da87b63bcd9f3de3d83d144bd177796dab92ae2e
loicBe21 <loicRavelo05@gmail.com>
2023-05-04 17:33:18 +0200

    firstcommit
    commit

---
+++ projectCorps/src/main/java/com/tpS6WebDesignMai2023/projectCorps/ProjectCorpsApplication.java
@@ -0,0 +1,13 @@
+package com.tpS6WebDesignMai2023.projectCorps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCorpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCorpsApplication.class, args);
+	}
+
+}
